7ce118ece955429d009467bfdf38200175fba5be
SV <svadivazhagu@wpi.edu>
2021-03-13 00:58:41 -0500

    just read the vax csv from the url in pandas.

--- state-extract.py
+++ state-extract.py
@@ -1,9 +1,14 @@
 # coding: utf-8
 import pandas as pd
+import requests
 
-vax_data = pd.read_csv("vax.csv")
+url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv'
 
 
+vax_data = pd.read_csv(url)
+
+
+#separate into state's own csvs
 def state_separate(data):
     for state in pd.unique(data['location']):
         data[data['location'] == state].to_csv("state_vax/" + state + ".csv", header=True,
@@ -12,14 +17,13 @@ def state_separate(data):
 
 
 
-# do it by date now instead of state
-
+#separate into ordered by date.
 def date_separate(data):
     for date in pd.unique(data['date']):
         data[data['date'] == date].to_csv("date_vax/" + date + ".csv", header=True,
                                           index=False)
 
 
-date_separate(vax_data)
+
 
 
